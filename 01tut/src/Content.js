c50ff47daecb0ecfc3b9db368bfdd24bcb6e31fb
menson431 <menso431@gmail.com>
2021-11-20 21:21:02 +1100

    Understanding Click Events

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -4,12 +4,24 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3 );
         return names[int];
     }
+
+    const handleClick = () => {
+        console.log('You Clicked..it')
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleNameChange()}!
             </p>
-            
+            <button onClick={handleClick}>Click it</button>
+            <button onClick={() => {handleClick2('Dave')}}>Click it Person</button>
+            <button onClick={(e) => {handleClick3(e)}}>Click it Event</button>
         </main>
     )
 }
